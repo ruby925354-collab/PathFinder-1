daccb2f845a6d06096c2e9b5550a3d90f781f151
Hyouka39 <140324627+Hyouka39@users.noreply.github.com>
2025-11-04 02:17:41 +0800

    Update database.py

--- database.py
+++ database.py
@@ -2,17 +2,18 @@ import mysql.connector
 from mysql.connector import pooling
 import os
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST", "switchyard.proxy.rlwy.net")
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "IceCream39")
+DB_PASS = os.getenv("DB_PASS", "TkcNZWDdisJfnNpUOYzJmFvfzlYBulMD")
 DB_NAME = os.getenv("DB_NAME", "pathfinder")
+DB_PORT = int(os.getenv("DB_PORT", 56665))  
 
-# ---------- DB helpers ----------
 DB_CONFIG = {
     "host": DB_HOST,
     "user": DB_USER,
     "password": DB_PASS,
     "database": DB_NAME,
+    "port": DB_PORT,
     "autocommit": False,
 }
 
