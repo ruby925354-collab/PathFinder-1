7f5d07b879eab3509d202ab6554519582cb21de4
Hyouka39 <140324627+Hyouka39@users.noreply.github.com>
2025-11-17 03:16:09 +0800

    Add files via upload

---
+++ app/chatbot/index.js
@@ -0,0 +1,6 @@
+export { whatTriggers } from "./whatTriggers.js";
+export { howTriggers } from "./howTriggers.js";
+export { whyTriggers } from "./whyTriggers.js";
+export { whoTriggers } from "./whoTriggers.js";
+// export { whereTriggers } from "./whereTriggers.js";
+// export { whenTriggers } from "./whenTriggers.js";
